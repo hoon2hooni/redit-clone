e35cf9526ef3142a2edd711b32d887e3e7843497
hoon2hooni <sanghoon.engineer@gmail.com>
2022-11-23 11:24:55 +0900

    :sparkles: add User entity

--- server/src/entities/User.ts
+++ server/src/entities/User.ts
@@ -1,18 +1,41 @@
-// import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+import { IsEmail, Length } from "class-validator";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  BaseEntity,
+  Index,
+  OneToMany,
+  BeforeInsert,
+} from "typeorm";
 
-// @Entity()
-// export class User {
+import bcrypt from "bcryptjs";
 
-//     @PrimaryGeneratedColumn()
-//     id: number
+@Entity()
+export default class User extends BaseEntity {
+  @Index()
+  @IsEmail(undefined, { message: "Invalid email" })
+  @Length(1, 255, { message: "Email too long" })
+  @Column({ unique: true })
+  email: string;
 
-//     @Column()
-//     firstName: string
+  @Index()
+  @Length(3, 32, { message: "Username too short" })
+  @Column()
+  username: string;
 
-//     @Column()
-//     lastName: string
+  @Column()
+  @Length(6, 255, { message: "Password too short" })
+  password: string;
 
-//     @Column()
-//     age: number
+  @OneToMany(() => Post, (post) => post.user)
+  posts: Post[];
 
-// }
+  @OneToMany(() => Vote, (vote) => vote.user)
+  votes: Vote[];
+
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 6);
+  }
+}
