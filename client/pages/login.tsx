9a6c591d0314d34cb0dd1b98e4263acde408c12e
hoon2hooni <sanghoon.engineer@gmail.com>
2022-11-24 11:41:36 +0900

    :sparkles: add login page

---
+++ client/pages/login.tsx
@@ -0,0 +1,67 @@
+import React, { FormEvent, useState } from "react";
+import InputGroup from "../components/InputGroup";
+import Link from "next/link";
+import axios from "axios";
+import { useRouter } from "next/router";
+
+const Login = () => {
+  let router = useRouter();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState<any>({});
+
+  // if (authenticated) router.push("/");
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+    try {
+      const res = await axios.post(
+        "/auth/login",
+        { password, username },
+        { withCredentials: true }
+      );
+
+      // dispatch("LOGIN", res.data?.user);
+
+      router.push("/");
+    } catch (error: any) {
+      console.log(error);
+      setErrors(error.response?.data || {});
+    }
+  };
+
+  return (
+    <div className="bg-white">
+      <div className="flex h-screen flex-col items-center justify-center p-6">
+        <div className="mx-auto w-10/12 md:w-96">
+          <h1 className="mb-2 text-lg font-medium">로그인</h1>
+          <form onSubmit={handleSubmit}>
+            <InputGroup
+              placeholder="Username"
+              value={username}
+              setValue={setUsername}
+              error={errors.username}
+            />
+            <InputGroup
+              placeholder="Password"
+              value={password}
+              setValue={setPassword}
+              error={errors.password}
+            />
+            <button className="mb-1 w-full rounded border border-gray-400 bg-gray-400 py-2 text-xs font-bold uppercase text-white">
+              로그인
+            </button>
+          </form>
+          <small>
+            아직 아이디가 없나요?
+            <Link href="/register" className="ml-1 uppercase text-blue-500">
+              회원가입
+            </Link>
+          </small>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
